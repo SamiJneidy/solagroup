abb10a8e68d701bd312beaa749bad0b1c51a9bd6
SamiJneidy <samihanijneidy@gmail.com>
2025-02-18 15:20:11 +0300

    removed drop all db objects

--- app/main.py
+++ app/main.py
@@ -17,7 +17,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 @app.get("/", status_code=status.HTTP_200_OK, tags=["Root"])
